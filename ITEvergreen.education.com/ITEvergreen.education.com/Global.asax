e565320ce6f50fb449c28268539c57896a64847c
Lishisan <18810539367@163.com>
2017-07-03 23:07:23 +0800

    搭框架
    先搭一个架子出来，后期可以任意改

---
+++ ITEvergreen.education.com/ITEvergreen.education.com/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="ITEvergreen.education.com.MvcApplication" Language="C#" %>
---
+++ ITEvergreen.education.com/ITEvergreen.education.com/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(ITEvergreen.education.com.Startup))]
+namespace ITEvergreen.education.com
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
